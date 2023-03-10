5d4d33a596cb388e8490d4f9d9263309ac65850d
Mv0sKff <Mv0sKff@protonmail.com>
2023-03-10 10:01:39 +0100

    wip first screen

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-#siehe https://www.youtube.com/watch?v=mEAjGChhwcs
-# https://sounds-mp3.com/
-
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.audio import SoundLoader
 
 class MyGridLayout(GridLayout):
-    pass
-        
+    pass        
 
 class MultimediaApp(App):
     def build(self):
-        self.title = 'Multimedia App'
+        self.title = 'Measure Mika'
         return MyGridLayout()
 
 if __name__ == "__main__":
