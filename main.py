f6b1fb9715dcb96fb4da42d3a122c9e24e4fc23e
Mv0sKff <Mv0sKff@protonmail.com>
2023-03-17 10:48:25 +0100

    add wip camera capture button

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.config import Config
+
+Config.set('graphics', 'resizable', True)
 
 class MainWindow(Screen):
     pass
-
 
 class SecondWindow(Screen):
     pass
