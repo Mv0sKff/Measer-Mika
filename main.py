249cedefb6d5d11f3333bd6ade632e9565a21efa
Mv0sKff <Mv0sKff@protonmail.com>
2023-03-20 00:16:31 +0100

    rotate camera

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ from kivy.lang import Builder
 from kivy.config import Config
 from kivy.utils import platform
 
-if platform == "android":
-    from android.permissions import request_permissions, Permission
-    request_permissions([
-        Permission.CAMERA,
-        Permission.WRITE_EXTERNAL_STORAGE,
-        Permission.READ_EXTERNAL_STORAGE
-    ])
-
 Config.set('graphics', 'resizable', True)
 
 class MainWindow(Screen):
@@ -25,26 +17,17 @@ class WindowManager(ScreenManager):
 
 class MeasureMikaApp(App):
     def build(self):
-        app = ScreenManager()
-        app.add_widget(MainWindow(name='main'))
-
         if platform == "android":
             from android.permissions import request_permissions, Permission
             request_permissions([
-                Permission.CAMERA,
-                Permission.WRITE_EXTERNAL_STORAGE,
-                Permission.READ_EXTERNAL_STORAGE
+                Permission.CAMERA
             ])
-            app.add_widget(SecondWindow(name='second'))
-
+        
+        app = ScreenManager()
+        app.add_widget(MainWindow(name='main'))
+        app.add_widget(SecondWindow(name='second'))
         
         return app
-
-def on_start(self):
-    if platform == "android":
-        from android.permissions import request_permissions, Permission
-        request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
-
 
 if __name__ == "__main__":
     MeasureMikaApp().run()
