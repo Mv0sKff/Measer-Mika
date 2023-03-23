3c281ac8ce103cbac7cf1d960deb9dc5f8fd3d9c
BobSty <78264479+BobSty-0@users.noreply.github.com>
2023-03-23 15:27:23 +0100

    Prüfung für die Größe eingebaut

--- main.py
+++ main.py
@@ -4,11 +4,25 @@ from kivy.uix.camera import Camera
 from kivy.lang import Builder
 from kivy.config import Config
 from kivy.utils import platform
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
 Config.set('graphics', 'resizable', True)
 
 class MainWindow(Screen):
-    pass
+    def checkData(self):
+        popup = Popup(title='Hinweis',
+            size_hint=(None, None), size=(350, 100))    
+
+        if self.ids.input.text == '':
+            popup.content = Label(text='Bitte geben Sie zuerst Ihre Größe in cm an!')
+            popup.open()
+            return True
+        if not self.ids.input.text.isdecimal():
+            popup.content = Label(text='Bitte geben Sie einen Numerischen Wert an!')
+            popup.open()
+            return True
+        return False
 
 class SecondWindow(Screen):
     def __init__(self, **kwargs):
