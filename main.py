d9fc950395c12e4cb311153d14ed2b14e28a5621
Mv0sKff <Mv0sKff@protonmail.com>
2023-03-26 13:38:22 +0200

    chore: remove unnecessary code

--- main.py
+++ main.py
@@ -57,13 +57,13 @@ class SecondWindow(Screen):
 
     def changeTippText(self):
         if self.schrittIndex == 1:
-            self.hinweis_label_text = 'Halten Sie das Handy auf Kopfhöhe und\nzielen Sie mit dem Fadenkreutz auf den tiefsten\nPunkt des Objektes. Nehmen Sie dann ein Foto auf.'
+            self.hinweis_label_text = 'Halten Sie das Handy auf Kopfhöhe und\nzielen Sie mit dem Fadenkreuz auf den tiefsten\nPunkt des Objektes. Nehmen Sie dann ein Foto auf.'
         elif self.schrittIndex == 2:
             self.hinweis_label_text = 'Super, erstes Foto ist gemacht, wenn Sie\nden Vorgang wiederholen möchten, dann benutzen\nSie den Button links unten. Wenn alles\npasst, dann den Button rechts unten.'
         elif self.schrittIndex == 3:
-            self.hinweis_label_text = 'Halten Sie das Handy wieder auf Kopfhöhe\nund zielen Sie mit dem Fadenkreutz auf den\nhöchsten Punkt des Objektes.'
+            self.hinweis_label_text = 'Halten Sie das Handy wieder auf Kopfhöhe\nund zielen Sie mit dem Fadenkreuz auf den\nhöchsten Punkt des Objektes.'
         elif self.schrittIndex == 4:
-            self.hinweis_label_text = 'Auch das ist geschaft, wenn Sie den Vorgang\nwiederholen möchten, dann benutzen Sie den\nButton links unten. Wenn alles passt,\ndann den Button rechts unten.'
+            self.hinweis_label_text = 'Auch das ist geschafft, wenn Sie den Vorgang\nwiederholen möchten, dann benutzen Sie den\nButton links unten. Wenn alles passt,\ndann den Button rechts unten.'
         else:
             self.hinweis_label_text = ''
 
@@ -77,17 +77,13 @@ class SecondWindow(Screen):
 
     def indexChanged(self):
         if self.schrittIndex == 1:
-            #self.disableWidget('go_back_redo_button', False)
             self.disableWidget('weiter_button', True)
             self.disableWidget('capture_button', False)
-            #self.createRedoButton(False)
             self.ids['go_back_redo_image'].source = 'images/ReturnButton.png'
             self.ids['camera'].play = True
         elif self.schrittIndex == 2:
-            #self.disableWidget('go_back_redo_button', True)
             self.disableWidget('weiter_button', False)
             self.disableWidget('capture_button', True)
-            #self.createRedoButton(True)
             self.ids['go_back_redo_image'].source = 'images/RedoButton.png'
             self.ids['camera'].play = False
         elif self.schrittIndex == 3:
@@ -100,31 +96,6 @@ class SecondWindow(Screen):
             # Hier muss dann der 3. Bildschirm aufgerufen werden
             pass
         self.changeTippText()
-    
-    def createRedoButton(self, create: bool = True):
-        if create:
-            print("create redoButton")
-            newWidget = Builder.load_string('''
-Button:
-	id: redo_button
-	size_hint: (None, None)
-	pos_hint: {"center_x":0.2, "center_y":0.1}
-    on_release:
-        app.root.children[0].returnSchrittIndex()
-	background_color: 0, 0, 0, 0
-	Image:
-		id: redo_image
-		source: 'images/RedoButton.png'
-		center_x: self.parent.center_x
-		center_y: self.parent.center_y
-''')
-            self.add_widget(newWidget)
-        else:
-            # does not work
-            print("try remove redoButton")
-            if 'redo_button' in self.ids:
-                print("remove redoButton")
-                self.remove_widget('redo_button')
 
 class ThirdWindow(Screen):
     pass
@@ -135,11 +106,14 @@ class FourthWindow(Screen):
         self.sensorEnabled = False
 
     def get_orientation(self, dt):
-        val = gyroscope.orientation
+        try:
+            val = gyroscope.orientation
 
-        self.ids.label1.text = "X: " + str(val[0])
-        self.ids.label2.text = "Y: " + str(val[1])
-        self.ids.label3.text = "Z: " + str(val[2])
+            self.ids.label1.text = "X: " + str(val[0])
+            self.ids.label2.text = "Y: " + str(val[1])
+            self.ids.label3.text = "Z: " + str(val[2])
+        except:
+            print("error gyroscope.orientation")
 
     def pressed1(self):
         try:
