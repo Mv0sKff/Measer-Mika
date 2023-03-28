1bf558bf5f255952cde9786bfb39dbd1e433a08c
BobSty <78264479+BobSty-0@users.noreply.github.com>
2023-03-28 22:00:48 +0200

    Download auf gut glück

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ class ThirdWindow(Screen):
             }
         if platform == "android":
             from android.storage import primary_external_storage_path
-            fname = os.path.join(primary_external_storage_path(),'ergebnisse.json')
+            fname = os.path.join(primary_external_storage_path(),'/Download/ergebnisse.json')
             with open(fname, 'wb') as f:        
                 f.write(dictionary)
             return fname
